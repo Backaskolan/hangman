a173f13cf92849570779c20a1bb397a6459607a2
Linus Rundberg Streuli <linus.rundberg@backaskolan.se>
2018-05-18 09:51:46 +0200

    added highscore sorting

--- test_highscores.py
+++ test_highscores.py
@@ -1,6 +1,8 @@
 import json
+from collections import OrderedDict
 
 with open('scores.json', 'r') as f:
 		scores = json.load(f)
 	
-print(scores)
+for r in sorted(scores, key=lambda t: t['highest_round'], reverse=True):
+	print('{}\t\t{}\t\t{}\t\t{}'.format(r['player_name'], r['highest_round'], r['lives'], r['difficulty']))
